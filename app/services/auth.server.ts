39619763635a9c83f9007963294c7f1b4286d9ba
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-08 21:53:00 +0200

    feat: create auth service

---
+++ app/services/auth.server.ts
@@ -0,0 +1,36 @@
+import { Authenticator } from "remix-auth";
+import { FormStrategy } from "remix-auth-form";
+import { sessionStorage } from "~/services/session.server";
+import { prisma } from "~/db/prisma.server";
+import bcrypt from "bcryptjs";
+import { User } from "~/@types/user";
+
+/** Create an instance of Authenticator */
+export let authenticator = new Authenticator<User>(sessionStorage);
+
+/** Registering a From strategy */
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    const email = form.get("email") as string;
+    const password = form.get("password") as string;
+
+    if (!email || !password) {
+      throw new Error("Email and password are required");
+    }
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    const isPassVerify = await bcrypt.compare(password, user.password);
+
+    if (!user || !isPassVerify) {
+      throw new Error("Invalid email or password");
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+  }),
+  "user-pass"
+);
