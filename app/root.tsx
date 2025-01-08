04fc3784d384a4ad1208c49ed9087ed45c83c2ce
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-08 21:40:24 +0200

    chore: add polaris provider to root

--- app/root.tsx
+++ app/root.tsx
@@ -5,41 +5,62 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import { authenticator } from "~/services/auth.server";
+import { sessionStorage } from "~/services/session.server";
+import { PolarisProvider } from "~/components/providers/PolarisProvider";
 
+import Header from "~/components/Header/Header";
+import styles from "@shopify/polaris/build/esm/styles.css";
 import "./tailwind.css";
 
+export const loader = async ({ request }: { request: Request }) => {
+  const session = await sessionStorage.getSession(
+    request.headers.get("cookie")
+  );
+  const user = session.get("user");
+  return json({ user });
+};
+
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
+  // { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  // {
+  //   rel: "preconnect",
+  //   href: "https://fonts.gstatic.com",
+  //   crossOrigin: "anonymous",
+  // },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: styles,
   },
+  //   {
+  //     rel: "stylesheet",
+  //     href: "@shopify/polaris/build/esm/styles.css",
+  //   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
+  const { user } = useLoaderData();
+
   return (
-    <html lang="en">
+    <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <PolarisProvider>
+          <Header user={user} />
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+        </PolarisProvider>
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
