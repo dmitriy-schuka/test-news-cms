aa17b3324cbe2ffd9082a3cd71590cfcb327bc47
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-13 21:18:48 +0200

    feat: create separate login route

---
+++ app/routes/login.tsx
@@ -0,0 +1,18 @@
+import { ActionFunction, redirect, json } from "@remix-run/node";
+import { authenticator } from "~/services/auth.server";
+import { sessionStorage } from "~/services/session.server";
+
+export async function action({ request }: { request: Request }) {
+  /** Authenticate the user via strategy */
+  const user = await authenticator.authenticate("user-pass", request);
+
+  const session = await sessionStorage.getSession(request.headers.get("cookie"));
+  session.set("user", user);
+
+  /** Save the session and redirect to the main page */
+  throw redirect("/app", {
+    headers: {
+      "Set-Cookie": await sessionStorage.commitSession(session),
+    },
+  });
+}
