c2c1d6330a91337dddfc72e694f5b2106931279b
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-13 21:18:18 +0200

    feat: create separate app route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,5 @@
-import type { MetaFunction, LoaderFunction, json } from "@remix-run/node";
-import { Card, Page, Button, TextField } from "@shopify/polaris";
-import Header from "~/components/Header/Header";
+import { redirect } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+export const loader = async () => {
+  return redirect(`/app`);
 };
-
-export default function Index() {
-  return (
-    <Page
-      title={"Fox News"}
-      fullWidth
-      titleHidden
-    >
-      {/*<Header />*/}
-      Hello!
-    </Page>
-  );
-}
