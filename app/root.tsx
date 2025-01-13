08fd3e89aa9c19c5966700adc57ec2b7051cfc3c
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-13 21:13:02 +0200

    feat: edit root

--- app/root.tsx
+++ app/root.tsx
@@ -5,24 +5,18 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-import { authenticator } from "~/services/auth.server";
-import { sessionStorage } from "~/services/session.server";
 import { PolarisProvider } from "~/components/providers/PolarisProvider";
 
-import Header from "~/components/Header/Header";
-import styles from "@shopify/polaris/build/esm/styles.css";
+// import styles from "@shopify/polaris/build/esm/styles.css";
+import "@shopify/polaris/build/esm/styles.css";
 import "./tailwind.css";
 
-export const loader = async ({ request }: { request: Request }) => {
-  const session = await sessionStorage.getSession(
-    request.headers.get("cookie")
-  );
-  const user = session.get("user");
-  return json({ user });
-};
+// export const loader = async ({ request }: { request: Request }) => {
+//   const session = await sessionStorage.getSession(request.headers.get("cookie"));
+//   const user = session.get("user");
+//   return json({ user });
+// };
 
 export const links: LinksFunction = () => [
   // { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -31,19 +25,18 @@ export const links: LinksFunction = () => [
   //   href: "https://fonts.gstatic.com",
   //   crossOrigin: "anonymous",
   // },
-  {
-    rel: "stylesheet",
-    href: styles,
-  },
+  // {
+  //   rel: "stylesheet",
+  //   href: styles,
+  // },
   //   {
   //     rel: "stylesheet",
   //     href: "@shopify/polaris/build/esm/styles.css",
   //   },
+  { rel: "stylesheet", href: "https://unpkg.com/@shopify/polaris/build/esm/styles.css" },
 ];
 
 export default function App() {
-  const { user } = useLoaderData();
-
   return (
     <html>
       <head>
@@ -55,7 +48,6 @@ export default function App() {
       </head>
       <body>
         <PolarisProvider>
-          <Header user={user} />
           <Outlet />
           <ScrollRestoration />
           <Scripts />
