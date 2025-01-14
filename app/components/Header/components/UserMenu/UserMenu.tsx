868c09109983b2475f3d0c067d083dea7990164e
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-14 16:32:15 +0200

    feat(UserMenu): add modals switcher

--- app/components/Header/components/UserMenu/UserMenu.tsx
+++ app/components/Header/components/UserMenu/UserMenu.tsx
@@ -1,33 +1,58 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Modal from 'react-modal';
 import { Button, Avatar } from "@shopify/polaris";
 
 import Login from '../Login/Login';
 import Logout from '../Logout/Logout';
+import Signup from '../Signup/Signup';
+import { getInitials } from "~/utils/common";
 
-const UserMenu = ({ user }: { user: { email: string, name: string } | null }) => {
+const UserMenu = ({ user }) => {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalType, setModalType] = useState("");
+
+  useEffect(() => {
+    user
+      ? setModalType("logout")
+      : setModalType("login")
+  }, [user, setModalType]);
 
   const openModal = useCallback(() => {
     setIsOpen(true);
-  }, []);
+  }, [setIsOpen]);
+
   const closeModal = useCallback(() => {
     setIsOpen(false);
-  }, []);
+  }, [setIsOpen]);
 
-  const initials = user?.name
-    ? user.name
-      .split(" ")
-      .map((n) => n[0])
-      .join("")
-    : "U";
+  const renderModal = useMemo(() => {
+    switch (modalType) {
+      case "signup":
+        return <Signup closeModal={closeModal}/>
+      case "logout":
+        return <Logout closeModal={closeModal}/>
+      case "login":
+        return <Login closeModal={closeModal} setModalType={setModalType}/>
+      default:
+        return null;
+    }
+  }, [modalType, setModalType, closeModal]);
+
+  // const initials = user?.name
+  //   ? user.name
+  //     .split(" ")
+  //     .map((n) => n[0])
+  //     .join("")
+  //   : "U";
+
+  const initials = getInitials(user?.firstName, user?.lastName);
 
   return (
     <div>
       <Button variant={"plain"} onClick={openModal} fullWidth size={"large"}>
         {
           user
-            ? <Avatar initials={initials} name={user.name || "User"} size={"xl"} />
+            ? <Avatar initials={initials} name={user?.firstName || "User"} size={"xl"} />
             : <Avatar customer size={"xl"} />
         }
       </Button>
@@ -35,7 +60,6 @@ const UserMenu = ({ user }: { user: { email: string, name: string } | null }) =>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        contentLabel="Example Modal"
         ariaHideApp={false}
         style={{
           overlay:{
@@ -46,10 +70,8 @@ const UserMenu = ({ user }: { user: { email: string, name: string } | null }) =>
           content: {
             border: "none",
             background: "none",
-
             width: "fit-content",
             height: "fit-content",
-
             top: '50%',
             left: '50%',
             right: 'auto',
@@ -59,13 +81,14 @@ const UserMenu = ({ user }: { user: { email: string, name: string } | null }) =>
           },
         }}
         closeTimeoutMS={300}
+        onAfterClose={() => {
+          user
+            ? setModalType("logout")
+            : setModalType("login")
+        }}
         // overlayClassName={styles.ReactModal__Overlay}
       >
-        {
-          user
-            ? <Logout closeModal={closeModal}/>
-            : <Login closeModal={closeModal}/>
-        }
+        {renderModal}
       </Modal>
     </div>
   )
