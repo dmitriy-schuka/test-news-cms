e0bd4d353b1de89524defae46114e74bd7b3571a
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-15 22:52:32 +0200

    feat: create check user auth service

---
+++ app/utils/checkUserAuth.server.ts
@@ -0,0 +1,11 @@
+import { redirect } from "@remix-run/node";
+import { sessionStorage } from "~/services/session.server";
+
+export const checkUserAuth = async (request) => {
+  const session = await sessionStorage.getSession(request.headers.get("cookie"));
+  const user = session.get("user");
+
+  if (!user) {
+    throw redirect("/app");
+  }
+}
