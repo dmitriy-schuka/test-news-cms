87c1bcde48b7f2fe7ca9cab55f023e7abc56fa3d
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-20 00:35:55 +0200

    feat: add route for tags table

---
+++ app/routes/app.tags.list/route.tsx
@@ -0,0 +1,55 @@
+import { useCallback } from "react";
+import { json, redirect } from "@remix-run/node";
+// import { ActionFunction, LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction, ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData, useNavigate } from "@remix-run/react";
+import { Page, InlineStack } from "@shopify/polaris";
+import TagsTable from "~/components/TagsTable/TagsTable";
+import { getTags } from "~/repositories/tagRepository.server";
+
+export const loader: LoaderFunction = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") ?? "1");
+
+  const fetchedTags = await getTags({
+    page,
+    sortOrder: url.searchParams.get("sort") as "asc" | "desc",
+    sortColumn: url.searchParams.get("column") ?? "id",
+  });
+
+  return json({ tagsData: fetchedTags });
+};
+
+export default function TagsList() {
+  const { tagsData } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+
+  const handleTagEdit = useCallback(
+    (id: number) => {
+      navigate(`/app/tag/${id}`)
+    },
+    [navigate],
+  );
+
+  return (
+    <Page
+      title={"Tags list"}
+      primaryAction={{
+        content: "Create tag",
+        onAction: () => navigate('/app/tag/new'),
+      }}
+    >
+      <InlineStack align={"center"}>
+        <TagsTable
+          tags={tagsData?.tags}
+          page={tagsData?.page}
+          handleTagEdit={handleTagEdit}
+          sortDirection={tagsData?.sortDirection}
+          sortColumn={tagsData?.sortColumn}
+          hasNextPage={tagsData?.hasNextPage}
+          hasPreviousPage={tagsData?.hasPreviousPage}
+        />
+      </InlineStack>
+    </Page>
+  );
+}
