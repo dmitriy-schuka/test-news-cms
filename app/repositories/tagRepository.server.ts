be2a03403aebea507853b986cfe341315d2c7a3b
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-21 00:36:14 +0200

    feat: change tag repository

--- app/repositories/tagRepository.server.ts
+++ app/repositories/tagRepository.server.ts
@@ -7,61 +7,85 @@ export const getTags = async (options?: {
   page?: number;
   sortOrder?: Prisma.SortOrder;
 }): Promise<Tag | null> => {
-  const page = options?.page ?? 1;
-  const sortDirection = options?.sortOrder ?? "desc";
-  const sortColumn = options?.sortColumn ?? "id";
-  const take = 10;
-  const skip = page > 1 ? take * (page - 1) : 0;
+  try {
+    const page = options?.page ?? 1;
+    const sortDirection = options?.sortOrder ?? "desc";
+    const sortColumn = options?.sortColumn ?? "id";
+    const take = 10;
+    const skip = page > 1 ? take * (page - 1) : 0;
 
-  const count = await prisma.tag.count();
-  const tags = await prisma.tag.findMany({
-    take,
-    skip,
-    orderBy: {
-      [sortColumn]: sortDirection,
-    },
-  });
+    const count = await prisma.tag.count();
+    const tags = await prisma.tag.findMany({
+      take,
+      skip,
+      orderBy: {
+        [sortColumn]: sortDirection,
+      },
+    });
 
-  return {
-    tags,
-    count,
-    page,
-    sortColumn,
-    sortDirection,
-    hasNextPage: count - skip > take,
-    hasPreviousPage: page > 1,
-  };
+    return {
+      tags,
+      count,
+      page,
+      sortColumn,
+      sortDirection,
+      hasNextPage: count - skip > take,
+      hasPreviousPage: page > 1,
+    };
+  } catch (err) {
+    throw new Error(`Error fetching tags: ${err}`);
+  }
 };
 
 export const getTagById = async (id: string): Promise<Tag | null> => {
-  return prisma.tag.findUnique({
-    where: { id },
-  });
+  try {
+    return prisma.tag.findUnique({
+      where: { id },
+    });
+  } catch (err) {
+    throw new Error(`Error fetching single tag: ${err}`);
+  }
 };
 
 export const getTagByName = async (name: string): Promise<Tag | null> => {
-  return prisma.tag.findUnique({
-    where: { name },
-  });
+  try {
+    return prisma.tag.findUnique({
+      where: { name },
+    });
+  } catch (err) {
+    throw new Error(`Error fetching single tag by name: ${err}`);
+  }
 };
 
 export const createTag = async (data: TagCreate): Promise<Tag> => {
-  return prisma.tag.create({
-    data,
-  });
+  try {
+    return prisma.tag.create({
+      data,
+    });
+  } catch (err) {
+    throw new Error(`Error creating tag: ${err}`);
+  }
 };
 
 export const updateTag = async (id: string, data: TagUpdate): Promise<Tag> => {
-  return prisma.tag.update({
-    where: { id },
-    data,
-  });
+  try {
+    return prisma.tag.update({
+      where: { id },
+      data,
+    });
+  } catch (err) {
+    throw new Error(`Error updating tag: ${err}`);
+  }
 };
 
 export const deleteTag = async (id: string): Promise<Tag> => {
-  return prisma.tag.delete({
-    where: { id },
-  });
+  try {
+    return prisma.tag.delete({
+      where: { id },
+    });
+  } catch (err) {
+    throw new Error(`Error deleting tag: ${err}`);
+  }
 };
 
 export const findOrCreateTag = async (
