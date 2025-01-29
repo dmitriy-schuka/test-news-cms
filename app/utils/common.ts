7e2ed8ade24588fb0a2fc29d5f6bc8686e829cc1
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-29 12:57:12 +0200

    feat: add new utils

--- app/utils/common.ts
+++ app/utils/common.ts
@@ -7,3 +7,12 @@ export const getInitials = (firstName: string, lastName: string): string => {
 export const checkIsArray = (arr: any[]): boolean => {
   return (arr && Array.isArray(arr) && arr.length > 0);
 };
+
+export const getFileNameFromUrl = (url: string | undefined): string => {
+  if (url) {
+    const parsedUrl = url.split('/');
+    return parsedUrl[parsedUrl.length - 1];
+  } else {
+    return null;
+  }
+}
