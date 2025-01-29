c4fedcaf45da49340c00ed25ef482f5ddc277c0b
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-29 13:54:40 +0200

    feat: add new models types

---
+++ app/@types/media.d.ts
@@ -0,0 +1,5 @@
+import type { Prisma, Media as PrismaMedia } from "@prisma/client";
+
+export type Media = PrismaMedia;
+export type MediaCreate = Prisma.MediaCreateInput;
+export type MediaUpdate = Prisma.MediaUpdateInput;
--- app/@types/news.d.ts
+++ app/@types/news.d.ts
@@ -1,5 +1,15 @@
 import type { Prisma, News as PrismaNews } from "@prisma/client";
 
+export type PaginatedNews = {
+  news: PrismaNews[];
+  count: number;
+  page: number;
+  sortColumn: string;
+  sortDirection: Prisma.SortOrder;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+};
+
 export type News = PrismaNews;
 export type NewsCreate = Prisma.NewsCreateInput;
 export type NewsUpdate = Prisma.NewsUpdateInput;
