8f71472eddbc187b7c4c486a3155bd2889a432b8
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-01-29 13:00:26 +0200

    feat(MediaInput): add initial media state

--- app/components/common/MediaInput/MediaInput.tsx
+++ app/components/common/MediaInput/MediaInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
   Button,
   BlockStack,
@@ -8,22 +8,34 @@ import {
   DropZone,
 } from "@shopify/polaris";
 import { NoteIcon } from '@shopify/polaris-icons';
+import { checkIsArray } from "~/utils/common";
+import { VALID_IMAGE_TYPES, VALID_VIDEO_TYPES } from "~/constants/common";
 
-const MediaInput = () => {
+const MediaInput = ({ newsData, handleChange }) => {
   const [files, setFiles] = useState<File[]>([]);
 
+  useEffect(() => {
+    if (newsData?.media && checkIsArray(newsData.media)) {
+
+      const newsFiles = newsData.media.map((media) => ({
+        url: media?.url,
+        type: media?.mediaType,
+      }));
+      setFiles(newsFiles);
+    }
+  }, [newsData]);
+
   const handleDropZoneDrop = useCallback(
-    (_dropFiles: File[], acceptedFiles: File[], _rejectedFiles: File[]) =>
-      setFiles([...acceptedFiles]),
-    [],
+    async (_dropFiles: File[], acceptedFiles: File[], _rejectedFiles: File[]) => {
+      setFiles([...acceptedFiles]);
+      handleChange(acceptedFiles ,'files');
+    },
+    [setFiles, handleChange],
   );
 
   const clearImages = useCallback(() => {
     setFiles([]);
   }, [setFiles]);
-
-  const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-  const validVideoTypes = ['video/mp4'];
 
   const fileUpload = !files.length && <DropZone.FileUpload />;
   const uploadedFiles = files.length > 0 && (
@@ -33,18 +45,18 @@ const MediaInput = () => {
           files.map((file, index) => (
             <InlineStack key={index} align={"center"} blockAlign={"center"}>
               {
-                validImageTypes.includes(file.type)
+                VALID_IMAGE_TYPES.includes(file.type)
                   ?
                     <img
-                      src={window.URL.createObjectURL(file)}
+                      src={file?.url ? file.url : window.URL.createObjectURL(file)}
                       alt={"Media image"}
                       style={{maxWidth: "373px", maxHeight: "118px"}}
                     />
                   :
-                    validVideoTypes.includes(file.type)
+                  VALID_VIDEO_TYPES.includes(file.type)
                       ?
                         <video
-                          src={window.URL.createObjectURL(file)}
+                          src={file?.url ? file.url : window.URL.createObjectURL(file)}
                           autoPlay
                           style={{maxWidth: "373px", maxHeight: "118px"}}
                         />
@@ -55,7 +67,6 @@ const MediaInput = () => {
                           style={{maxWidth: "373px", maxHeight: "118px"}}
                         />
               }
-
 
               {/*<Text variant="bodySm" as="p">*/}
               {/*  {file.type}*/}
