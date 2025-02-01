745ed926e27cdf3fc6357ef2a5909ced6a94a731
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-01 20:36:23 +0200

    feat: edit fetch all news in news api

--- app/repositories/newsRepository.server.ts
+++ app/repositories/newsRepository.server.ts
@@ -4,8 +4,7 @@ import type { News, PaginatedNews, NewsCreate, NewsUpdate } from "~/@types/news"
 import type { Tag } from "~/@types/tag";
 import { deleteFile } from "~/services/minio.server";
 
-export const getNews = async (
-  newsTags?: number[],
+export const getNews = async (_,
   options?: {
     sortColumn?: string;
     page?: number;
@@ -20,12 +19,69 @@ export const getNews = async (
     const take = 10;
     const skip = page > 1 ? take * (page - 1) : 0;
 
+    const totalNews = await prisma.news.count();
+
+    const news = await prisma.news.findMany({
+      take,
+      skip,
+      orderBy: {
+        [sortColumn]: sortDirection,
+      },
+      include: {
+        tags: true,
+        media: true,
+      },
+    });
+
+    return {
+      news,
+      totalNews,
+      page,
+      sortColumn,
+      sortDirection,
+      hasNextPage: totalNews - skip > take,
+      hasPreviousPage: page > 1,
+    };
+  } catch (err) {
+    throw new Error(`Error fetching news: ${err}`);
+  }
+};
+
+export const getMainPageNews = async (
+  newsTags?: number[],
+  options?: {
+    sortColumn?: string;
+    page?: number;
+    sortOrder?: Prisma.SortOrder;
+  },
+): Promise<PaginatedNews | null> => {
+  try {
+    const page = options?.page ?? 1;
+    const sortDirection = options?.sortOrder ?? "desc";
+    const sortColumn = options?.sortColumn ?? "id";
+    const take = 8;
+    const skip = page > 1 ? take * (page - 1) : 0;
+
+    const latestNews = await prisma.news.findFirst({
+      where: {
+        published: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        tags: true,
+        media: true,
+      },
+    });
+
     let totalNews;
     let news;
 
     if (newsTags && Array.isArray(newsTags) && newsTags.length > 0) {
       const query = {
         where: {
+          published: true,
           tags: {
             some: {
               id: {
@@ -54,6 +110,12 @@ export const getNews = async (
       totalNews = await prisma.news.count();
 
       news = await prisma.news.findMany({
+        where: {
+          published: true,
+          id: {
+            not: latestNews?.id,
+          },
+        },
         take,
         skip,
         orderBy: {
@@ -68,6 +130,7 @@ export const getNews = async (
 
     return {
       news,
+      latestNews,
       totalNews,
       page,
       sortColumn,
@@ -76,7 +139,7 @@ export const getNews = async (
       hasPreviousPage: page > 1,
     };
   } catch (err) {
-    throw new Error(`Error fetching news: ${err}`);
+    throw new Error(`Error fetching all news: ${err}`);
   }
 };
 
