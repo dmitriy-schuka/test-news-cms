5ed7c541fca032df1b5157db69616f383affd30c
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-02 21:49:50 +0200

    feat: add new constants

--- app/constants/common.ts
+++ app/constants/common.ts
@@ -2,3 +2,24 @@ export const SALT_ROUNDS = 10;
 
 export const VALID_IMAGE_TYPES = ['image/gif', 'image/jpg', 'image/jpeg', 'image/png'];
 export const VALID_VIDEO_TYPES = ['video/mp4'];
+
+export const NEWS_MENU_ITEMS = [
+  {
+    title: 'All news',
+    redirectUrl: '/app/news/grid'
+  },
+  {
+    title: 'Search page',
+    redirectUrl: '/app/news/grid'
+  },
+  {
+    title: 'Search by tags page',
+    redirectUrl: '/app/news/grid'
+  }
+];
+
+export const DEVICES_BREAKPOINTS = {
+  mobile: 576,
+  tablet: 768,
+  desktop: 1024,
+};
