c79d43a759b9117c99802580cf36108ce37187f5
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-10 21:29:50 +0200

    feat(NewsGrid): edit news grid

--- app/components/NewsGrid/NewsGrid.tsx
+++ app/components/NewsGrid/NewsGrid.tsx
@@ -1,67 +1,15 @@
-import React, { useCallback, useMemo } from 'react';
-import { Tag, Box, Grid, Card, Text, BlockStack, InlineStack, Thumbnail, VideoThumbnail } from "@shopify/polaris";
-import { checkIsArray, formatTimeAgo } from "~/utils/common";
-import { VALID_IMAGE_TYPES, VALID_VIDEO_TYPES } from "~/constants/common";
+import React, { useMemo } from 'react';
+import { Box, Grid, BlockStack } from "@shopify/polaris";
 import NewsMediaCard from "~/components/NewsGrid/components/NewsMediaCard/NewsMediaCard";
-import DefaultImage from "~/assets/images/default-image.jpg";
-import styles from './NewsGrid.module.css';
 import NewsPagination from "~/components/NewsGrid/components/NewsPagination/NewsPagination";
 import BlockTitle from "~/components/common/BlockTitle/BlockTitle";
 import { useNavigate } from "@remix-run/react";
 import InjectionCard from "~/components/InjectionCard/InjectionCard";
+import NewsCard from "~/components/NewsCard/NewsCard";
 
 const NewsGrid = (props) => {
   const { news, latestNews, onSort, onNextPage, onPrevPage, hasNextPage, hasPrevPage, totalNews, page, handleFilterByTags } = props;
   const navigate = useNavigate();
-
-  const renderNewsMedia = useCallback((newsMedia) => {
-    return (
-      newsMedia?.map((mediaItem, index) => {
-        const mediaUrl = mediaItem?.url || DefaultImage;
-
-        switch (true) {
-          case VALID_IMAGE_TYPES.includes(mediaItem?.mediaType):
-            return (
-              <div key={`${index}-${mediaItem?.url}`} className={styles.News__media}>
-                <img
-                  src={mediaUrl}
-                  alt="News media"
-                  style={{/*maxWidth: "fit-content",*/ maxHeight: "inherit"}}
-                />
-              </div>
-            )
-          case VALID_VIDEO_TYPES.includes(mediaItem?.mediaType):
-            return (
-              <div key={`${index}-${mediaItem?.url}`} className={styles.News__media}>
-                <BlockStack inlineAlign={"start"} align={"center"}>
-                  <video
-                    src={mediaUrl}
-                    autoPlay
-                    loop
-                    className={styles.News__media}
-                  />
-                </BlockStack>
-              </div>
-            )
-          default:
-            return null;
-        }
-      })
-    );
-  }, [news]);
-
-  const renderNewsTags = useCallback((tags) => {
-    return (
-      tags?.map((tag, index) => (
-        <Tag
-          key={`${index}_${tag?.name}`} url="#"
-          onClick={() => handleFilterByTags([tag?.id])}
-        >
-          {tag?.name}
-        </Tag>
-      ))
-    )
-  }, [news, handleFilterByTags]);
 
   const renderNewsCards = useMemo(() => {
     return (
@@ -70,41 +18,8 @@ const NewsGrid = (props) => {
           <Grid.Cell key={`${index}_${newsItem?.title}_news`}>
             {
               newsItem?.hasOwnProperty("injectionType")
-                ? <InjectionCard itemData={newsItem}/>
-                :
-                  <Card>
-                    <BlockStack gap={200}>
-                      <div className={styles.NewsCard__wrapper} onClick={() => navigate(`/app/publication/${newsItem?.id}`)}>
-                        <BlockStack gap={200}>
-                          <Text variant="headingLg" as="h5">
-                            {newsItem?.title}
-                          </Text>
-
-                          {
-                            checkIsArray(newsItem?.media) && renderNewsMedia(newsItem.media)
-                          }
-
-                          <Text variant="bodyLg" as="p">
-                        <span className={styles.News__content}>
-                          {newsItem?.content}
-                        </span>
-                          </Text>
-                        </BlockStack>
-                      </div>
-
-                      <InlineStack gap={100}>
-                        {
-                          checkIsArray(newsItem?.tags) && renderNewsTags(newsItem.tags)
-                        }
-                      </InlineStack>
-
-                      <Text variant="bodySm" as="p">
-                        {
-                          formatTimeAgo(newsItem?.createdAt)
-                        }
-                      </Text>
-                    </BlockStack>
-                  </Card>
+                ? <InjectionCard itemData={newsItem} handleFilterByTags={handleFilterByTags}/>
+                : <NewsCard newsItem={newsItem} handleFilterByTags={handleFilterByTags}/>
             }
           </Grid.Cell>
         )
