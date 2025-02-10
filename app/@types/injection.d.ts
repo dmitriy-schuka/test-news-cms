6ed66575ed3693df98602375d0b0e5a472786480
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-10 16:51:03 +0200

    feat: create injections types

---
+++ app/@types/injection.d.ts
@@ -0,0 +1,5 @@
+import type { Prisma, Injection as PrismaInjection } from "@prisma/client";
+
+export type Injection = PrismaInjection;
+export type InjectionCreate = Prisma.InjectionCreateInput;
+export type InjectionUpdate = Prisma.InjectionUpdateInput;
