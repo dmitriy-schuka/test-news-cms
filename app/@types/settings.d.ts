7d9c20d53c1381f3d6b6079948eccf80187e87f3
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-10 16:56:08 +0200

    feat: create settings types

---
+++ app/@types/settings.d.ts
@@ -0,0 +1,5 @@
+import type { Prisma, Settings as PrismaSettings } from "@prisma/client";
+
+export type Settings = PrismaSettings;
+export type SettingsCreate = Prisma.SettingsCreateInput;
+export type SettingsUpdate = Prisma.SettingsUpdateInput;
