f2b1ed87f3f5fa544cdf6614e092019fbdd00d89
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-10 21:30:30 +0200

    feat: create injections list route

---
+++ app/routes/app.injections.list/route.tsx
@@ -0,0 +1,57 @@
+import { useCallback } from "react";
+import { json, redirect } from "@remix-run/node";
+// import { ActionFunction, LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction, ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData, useNavigate } from "@remix-run/react";
+import { Page, InlineStack } from "@shopify/polaris";
+import { getAllInjections } from "~/repositories/injectionRepository.server";
+import InjectionsTable from "~/components/InjectionsTable/InjectionsTable";
+
+export const loader: LoaderFunction = async ({ request }: LoaderFunctionArgs) => {
+  //await checkUserAuth(request);
+
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") ?? "1");
+
+  const fetchedInjections = await getAllInjections({
+    page,
+    sortOrder: url.searchParams.get("sort") as "asc" | "desc",
+    sortColumn: url.searchParams.get("column") ?? "id",
+  });
+
+  return json({ injectionsData: fetchedInjections });
+};
+
+export default function InjectionsList() {
+  const { injectionsData } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+
+  const handleInjectionEdit = useCallback(
+    (id: number) => {
+      navigate(`/app/injection/${id}`)
+    },
+    [navigate],
+  );
+
+  return (
+    <Page
+      title={"Injections list"}
+      primaryAction={{
+        content: "Create injection",
+        onAction: () => navigate('/app/injection/new'),
+      }}
+    >
+      <InlineStack align={"center"}>
+        <InjectionsTable
+          injections={injectionsData?.injections}
+          page={injectionsData?.page}
+          handleInjectionEdit={handleInjectionEdit}
+          sortDirection={injectionsData?.sortDirection}
+          sortColumn={injectionsData?.sortColumn}
+          hasNextPage={injectionsData?.hasNextPage}
+          hasPreviousPage={injectionsData?.hasPreviousPage}
+        />
+      </InlineStack>
+    </Page>
+  );
+}
