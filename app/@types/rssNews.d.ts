eb343c9075b7fd78afe04f905694469ba7e2ba13
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-19 01:39:05 +0200

    feat: create types for rss news and sources

---
+++ app/@types/rssNews.d.ts
@@ -0,0 +1,5 @@
+import type { Prisma, RssNews as PrismaRssNews } from "@prisma/client";
+
+export type RssNews = PrismaRssNews;
+export type RssNewsCreate = Prisma.RssNewsCreateInput;
+export type RssNewsUpdate = Prisma.RssNewsUpdateInput;
---
+++ app/@types/rssSource.d.ts
@@ -0,0 +1,5 @@
+import type { Prisma, RssSource as PrismaRssSource } from "@prisma/client";
+
+export type RssSource = PrismaRssSource;
+export type RssSourceCreate = Prisma.RssSourceCreateInput;
+export type RssSourceUpdate = Prisma.RssSourceUpdateInput;
