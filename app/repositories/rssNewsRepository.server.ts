001d3d0b4a2a13ecd4fcc9a127f884c437104a9e
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-19 01:41:47 +0200

    feat: create rss news repository

---
+++ app/repositories/rssNewsRepository.server.ts
@@ -0,0 +1,45 @@
+import { prisma } from "~/db/prisma.server";
+import type { RssNews, RssNewsCreate, RssNewsUpdate } from "~/@types/rssNews";
+import { extractMediaUrl } from "~/utils/extractMediaUrl";
+import { updateRssSource } from "~/repositories/rssSourceRepository.server";
+
+export const getAllRssNews = async () => {
+  try {
+    return await prisma.rssNews.findMany({
+      take: 2,
+    });
+  } catch (err) {
+    throw new Error(`Error fetching all rss news: ${err}`);
+  }
+};
+
+export const deleteRssNewsBySourceId = async (rssSourceId: number) => {
+  try {
+    await prisma.rssNews.deleteMany({
+      where: {
+        rssSourceId,
+      },
+    });
+
+    await updateRssSource(rssSourceId, {fetchedAt: new Date()});
+  } catch (err) {
+    throw new Error(`Error deleting rss news: ${err}`);
+  }
+};
+
+export const storeImportedRssNews = async (data: RssNewsUpdate, rssSourceId: number) => {
+  try {
+    if (data?.mediaUrl) {
+      data.mediaUrl = extractMediaUrl(data.mediaUrl);
+    }
+
+    await prisma.rssNews.create({
+      data: {
+        ...data,
+        rssSourceId,
+      },
+    });
+  } catch (err) {
+    throw new Error(`Error in store rss news: ${err}`);
+  }
+};
