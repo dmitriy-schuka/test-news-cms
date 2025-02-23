da7c2ec871c4aed1ea883410a3d008068c6018ff
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-23 20:10:30 +0200

    feat(BlockTitle): edit types

--- app/components/common/BlockTitle/BlockTitle.tsx
+++ app/components/common/BlockTitle/BlockTitle.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import type {FC} from "react";
 import { Text } from "@shopify/polaris";
 
-const BlockTitle = ({title}) => {
+interface IBlockTitleProps {
+  title: string;
+}
+
+const BlockTitle: FC<IBlockTitleProps> = ({title}) => {
   return (
     <Text variant="headingSm" as="h6">
       {title}
