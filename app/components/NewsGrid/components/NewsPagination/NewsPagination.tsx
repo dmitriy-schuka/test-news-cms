3ea1ca3133345f410b21095fc9537f7f97553916
dmitriy-schuka <dmitriy.schuka@devit.group>
2025-02-23 13:04:55 +0200

    feat(NewsPagination): add new styles and types

--- app/components/NewsGrid/components/NewsPagination/NewsPagination.tsx
+++ app/components/NewsGrid/components/NewsPagination/NewsPagination.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import type {FC} from "react";
 import { InlineStack, Pagination } from "@shopify/polaris";
+import styles from './NewsPagination.module.css';
 
-const NewsPagination = ({onNextPage, onPrevPage, hasNextPage, hasPrevPage, totalNews, page}) => {
-  const maxPage = Math.ceil((Number(totalNews) / 8));
+interface INewsPaginationProps {
+  onNextPage: () => void;
+  onPrevPage: () => void;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+  totalNews: number;
+  page: number;
+}
+
+const NewsPagination: FC<INewsPaginationProps> = ({onNextPage, onPrevPage, hasNextPage, hasPrevPage, totalNews, page}) => {
+  const maxPage = Math.max(1, Math.ceil(totalNews / 8));
   const paginationLabel = `Page ${page} of ${maxPage}`;
 
   return (
@@ -14,7 +25,11 @@ const NewsPagination = ({onNextPage, onPrevPage, hasNextPage, hasPrevPage, total
         hasNext={hasNextPage}
         hasPrevious={hasPrevPage}
         label={paginationLabel}
-      />
+      >
+        <span aria-live="polite" className={styles.PaginationLabel__hidden}>
+          {paginationLabel}
+        </span>
+      </Pagination>
     </InlineStack>
   );
 };
